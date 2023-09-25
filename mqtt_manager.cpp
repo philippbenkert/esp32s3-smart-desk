a99299c04e47ebe77b81ba7dbea4144ee81d536d
philippbenkert <76922498+philippbenkert@users.noreply.github.com>
2023-09-25 17:33:56 +0200

    Update mqtt_manager.cpp

--- mqtt_manager.cpp
+++ mqtt_manager.cpp
@@ -1,1 +1,34 @@
+#include "mqtt_manager.h"
+
+MQTTManager::MQTTManager(const char* mqtt_server, int mqtt_port, const char* client_id) 
+    : _client(_espClient) {
+    _client.setServer(mqtt_server, mqtt_port);
+    _client.setClientId(client_id);
+}
+
+void MQTTManager::setup() {
+    while (!_client.connected()) {
+        Serial.println("Connecting to MQTT...");
+
+        if (_client.connect(_client.getClientId())) {
+            Serial.println("Connected to MQTT");
+        } else {
+            Serial.print("Failed with state ");
+            Serial.print(_client.state());
+            delay(2000);
+        }
+    }
+}
+
+void MQTTManager::loop() {
+    _client.loop();
+}
+
+bool MQTTManager::isConnected() const {
+    return _client.connected();
+}
+
+void MQTTManager::setCallback(void (*callback)(char*, byte*, unsigned int)) {
+    _client.setCallback(callback);
+}
 
