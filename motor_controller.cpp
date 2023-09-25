ef22871d3c1ce2205bbee0edc3bb1a7d53f56930
philippbenkert <76922498+philippbenkert@users.noreply.github.com>
2023-09-25 17:50:50 +0200

    Update motor_controller.cpp

--- motor_controller.cpp
+++ motor_controller.cpp
@@ -1,1 +1,29 @@
 
+#include "motor_controller.h"
+
+MotorController::MotorController(RelayController& relayController)
+    : _relayController(relayController), _motorRunTimeUp(0), _motorRunTimeDown(0) {}
+
+void MotorController::setup() {
+    // Hier können Sie Initialisierungscode hinzufügen, falls erforderlich.
+}
+
+void MotorController::moveTableUp() {
+    _relayController.setRelayState(RelayController::TABLE_UP, true);
+    delay(_motorRunTimeUp);
+    _relayController.setRelayState(RelayController::TABLE_UP, false);
+}
+
+void MotorController::moveTableDown() {
+    _relayController.setRelayState(RelayController::TABLE_DOWN, true);
+    delay(_motorRunTimeDown);
+    _relayController.setRelayState(RelayController::TABLE_DOWN, false);
+}
+
+void MotorController::setMotorRunTimeUp(unsigned long duration) {
+    _motorRunTimeUp = duration;
+}
+
+void MotorController::setMotorRunTimeDown(unsigned long duration) {
+    _motorRunTimeDown = duration;
+}
