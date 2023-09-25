b15ed687ae8b62c75cc1a8bcebf60915accf27f0
philippbenkert <76922498+philippbenkert@users.noreply.github.com>
2023-09-25 17:42:20 +0200

    Update relay_controller.cpp

--- relay_controller.cpp
+++ relay_controller.cpp
@@ -1,1 +1,27 @@
+#include "relay_controller.h"
+
+RelayController::RelayController(int serPin, int rclkPin, int srclkPin, int numOfShiftRegisters)
+    : _shiftRegister(numOfShiftRegisters, serPin, rclkPin, srclkPin) {
+    _relayStates.fill(false);
+}
+
+void RelayController::setup() {
+    for (int i = 0; i < 6; i++) {
+        _shiftRegister.set(i, LOW);
+    }
+}
+
+void RelayController::setRelayState(int relayIndex, bool state) {
+    if (relayIndex >= 0 && relayIndex < 6) {
+        _relayStates[relayIndex] = state;
+        _shiftRegister.set(relayIndex, state ? HIGH : LOW);
+    }
+}
+
+bool RelayController::getRelayState(int relayIndex) const {
+    if (relayIndex >= 0 && relayIndex < 6) {
+        return _relayStates[relayIndex];
+    }
+    return false;
+}
 
