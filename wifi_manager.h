c79c009ae005c65913d352333e0bf32e9a3a1295
philippbenkert <76922498+philippbenkert@users.noreply.github.com>
2023-09-25 17:28:07 +0200

    Update wifi_manager.h

--- wifi_manager.h
+++ wifi_manager.h
@@ -1,1 +1,20 @@
+#ifndef WIFI_MANAGER_H
+#define WIFI_MANAGER_H
 
+#include <WiFi.h>
+
+class WiFiManager {
+public:
+    WiFiManager(const char* ssid, const char* password);
+
+    void setup();
+    void loop();
+
+    bool isConnected() const;
+
+private:
+    const char* _ssid;
+    const char* _password;
+};
+
+#endif // WIFI_MANAGER_H
