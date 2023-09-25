041ac0bfe56c8acc9f0b91863a2244bbbdeaaecb
philippbenkert <76922498+philippbenkert@users.noreply.github.com>
2023-09-25 17:33:27 +0200

    Update mqtt_manager.h

--- mqtt_manager.h
+++ mqtt_manager.h
@@ -1,1 +1,24 @@
+#ifndef MQTT_MANAGER_H
+#define MQTT_MANAGER_H
+
+#include <PubSubClient.h>
+#include <WiFi.h>
+
+class MQTTManager {
+public:
+    MQTTManager(const char* mqtt_server, int mqtt_port, const char* client_id);
+
+    void setup();
+    void loop();
+
+    bool isConnected() const;
+
+    void setCallback(void (*callback)(char*, byte*, unsigned int));
+
+private:
+    WiFiClient _espClient;
+    PubSubClient _client;
+};
+
+#endif // MQTT_MANAGER_H
 
