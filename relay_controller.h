2932aa24270d1bafcd23b705e41993af51f02543
philippbenkert <76922498+philippbenkert@users.noreply.github.com>
2023-09-25 17:41:47 +0200

    Update relay_controller.h

--- relay_controller.h
+++ relay_controller.h
@@ -1,1 +1,30 @@
+#ifndef RELAY_CONTROLLER_H
+#define RELAY_CONTROLLER_H
+
+#include <ShiftRegister74HC595.h>
+
+class RelayController {
+public:
+    RelayController(int serPin, int rclkPin, int srclkPin, int numOfShiftRegisters);
+
+    void setup();
+
+    void setRelayState(int relayIndex, bool state);
+    bool getRelayState(int relayIndex) const;
+
+    enum Relay {
+        MONITOR = 0,
+        LAPTOP,
+        TABLE_UP,
+        TABLE_DOWN,
+        RESERVE1,
+        RESERVE2
+    };
+
+private:
+    ShiftRegister74HC595<numOfShiftRegisters> _shiftRegister;
+    std::array<bool, 6> _relayStates;
+};
+
+#endif // RELAY_CONTROLLER_H
 
