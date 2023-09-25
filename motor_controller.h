eea2ffad524171ceb41da8d069b76d8ac5c87e27
philippbenkert <76922498+philippbenkert@users.noreply.github.com>
2023-09-25 17:50:29 +0200

    Update motor_controller.h

--- motor_controller.h
+++ motor_controller.h
@@ -1,1 +1,27 @@
+#ifndef MOTOR_CONTROLLER_H
+#define MOTOR_CONTROLLER_H
+
+#include "relay_controller.h"
+
+class MotorController {
+public:
+    MotorController(RelayController& relayController);
+
+    void setup();
+
+    // Bewegt die Tischplatte nach oben oder unten
+    void moveTableUp();
+    void moveTableDown();
+
+    // Setzt die Laufzeit der Motoren
+    void setMotorRunTimeUp(unsigned long duration);
+    void setMotorRunTimeDown(unsigned long duration);
+
+private:
+    RelayController& _relayController;
+    unsigned long _motorRunTimeUp;
+    unsigned long _motorRunTimeDown;
+};
+
+#endif // MOTOR_CONTROLLER_H
 
