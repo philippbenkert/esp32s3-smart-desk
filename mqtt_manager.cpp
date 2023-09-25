79f8d74a3cef4f25553a2539cbc73084a7742f6c
philippbenkert <76922498+philippbenkert@users.noreply.github.com>
2023-09-25 17:53:52 +0200

    Update mqtt_manager.cpp

--- mqtt_manager.cpp
+++ mqtt_manager.cpp
@@ -1,34 +1,56 @@
 #include "mqtt_manager.h"
 
-MQTTManager::MQTTManager(const char* mqtt_server, int mqtt_port, const char* client_id) 
-    : _client(_espClient) {
-    _client.setServer(mqtt_server, mqtt_port);
-    _client.setClientId(client_id);
-}
+MQTTManager::MQTTManager(WiFiManager& wifiManager, RelayController& relayController, MotorController& motorController)
+    : _wifiManager(wifiManager), _relayController(relayController), _motorController(motorController), _client(_wifiManager.getClient()) {}
 
 void MQTTManager::setup() {
-    while (!_client.connected()) {
-        Serial.println("Connecting to MQTT...");
-
-        if (_client.connect(_client.getClientId())) {
-            Serial.println("Connected to MQTT");
-        } else {
-            Serial.print("Failed with state ");
-            Serial.print(_client.state());
-            delay(2000);
-        }
-    }
+    _client.setServer(MQTT_SERVER, MQTT_PORT);
+    _client.setCallback([this](char* topic, byte* payload, unsigned int length) {
+        this->callback(topic, payload, length);
+    });
 }
 
 void MQTTManager::loop() {
+    if (!_client.connected()) {
+        // Hier können Sie einen Reconnect-Versuch hinzufügen
+    }
     _client.loop();
 }
 
-bool MQTTManager::isConnected() const {
-    return _client.connected();
+void MQTTManager::callback(char* topic, byte* payload, unsigned int length) {
+    char payloadStr[length + 1];
+    strncpy(payloadStr, (char*)payload, length);
+    payloadStr[length] = '\0';
+
+    if (strncmp(topic, "/relay/", 7) == 0) {
+        handleRelayCommands(topic, payloadStr);
+    } else if (strncmp(topic, "/motor/", 7) == 0) {
+        handleMotorCommands(topic, payloadStr);
+    }
+    // Weitere Topics können hier hinzugefügt werden
 }
 
-void MQTTManager::setCallback(void (*callback)(char*, byte*, unsigned int)) {
-    _client.setCallback(callback);
+void MQTTManager::handleRelayCommands(const char* topic, const char* payload) {
+    int relayIndex = topic[7] - '1';
+    if (relayIndex >= 0 && relayIndex < 6) {
+        bool state = (strcmp(payload, "true") == 0);
+        _relayController.setRelayState(static_cast<RelayController::Relay>(relayIndex), state);
+        _client.publish(topic, state ? "true" : "false");
+    }
 }
 
+void MQTTManager::handleMotorCommands(const char* topic, const char* payload) {
+    if (strcmp(topic, "/motor/tableUp") == 0) {
+        _motorController.moveTableUp();
+        _client.publish(topic, "moving up");
+    } else if (strcmp(topic, "/motor/tableDown") == 0) {
+        _motorController.moveTableDown();
+        _client.publish(topic, "moving down");
+    } else if (strcmp(topic, "/motor/setRunTimeUp") == 0) {
+        _motorController.setMotorRunTimeUp(atoi(payload));
+        _client.publish(topic, payload);
+    } else if (strcmp(topic, "/motor/setRunTimeDown") == 0) {
+        _motorController.setMotorRunTimeDown(atoi(payload));
+        _client.publish(topic, payload);
+    }
+}
