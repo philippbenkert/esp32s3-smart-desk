861bc0e8f1e216c67e25060ff9b72d9da0490cb9
philippbenkert <76922498+philippbenkert@users.noreply.github.com>
2023-09-26 20:05:44 +0200

    Update mqtt_manager.h

--- mqtt_manager.h
+++ mqtt_manager.h
@@ -8,19 +8,21 @@
 
 class MQTTManager {
 public:
-    MQTTManager(WiFiManager& wifiManager, RelayController& relayController, MotorController& motorController);
-
+    MQTTManager(PubSubClient& client, WiFiManager& wifiManager, RelayController& relayController, MotorController& motorController, const char* mqtt_server, int mqtt_port);
+    bool publish(const char* topic, const char* payload);
+    void setCallback(void (*callback)(char*, byte*, unsigned int));
     void setup();
     void loop();
-
     void callback(char* topic, byte* payload, unsigned int length);
 
 private:
+    const char* _mqtt_server;
+    int _mqtt_port;
     WiFiManager& _wifiManager;
     RelayController& _relayController;
     MotorController& _motorController;
-    PubSubClient _client;
-
+    PubSubClient& _client;
+    void reconnect();
     void handleRelayCommands(const char* topic, const char* payload);
     void handleMotorCommands(const char* topic, const char* payload);
 };
