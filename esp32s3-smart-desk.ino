c82b3111ea68c2de7fc8bb4b1ff687fb9845221b
philippbenkert <76922498+philippbenkert@users.noreply.github.com>
2023-09-26 20:04:47 +0200

    Update esp32s3-smart-desk.ino

--- esp32s3-smart-desk.ino
+++ esp32s3-smart-desk.ino
@@ -2,68 +2,54 @@
 #include "mqtt_manager.h"
 #include "relay_controller.h"
 #include "motor_controller.h"
+#include <PubSubClient.h>  // Fügen Sie diese Zeile am Anfang der Datei hinzu
 
-const char* ssid = "Ihr_SSID";
-const char* password = "Ihr_Passwort";
+WiFiClient espClient;
+PubSubClient client(espClient);
+const char* ssid = "ssid";
+const char* password = "password";
 WiFiManager wifiManager(ssid, password);
 const char* mqtt_server = "192.168.0.1";
-const int mqtt_port = 1883;
-const char* client_id = "ESP32S3_Client";
-MQTTManager mqttManager(wifiManager, relayController, motorController);
+const int mqtt_port = 1024;
+const char* client_id = "ESP32S3_Schreibtisch";
 const int SER_Pin = 7;
 const int RCLK_Pin = 5;
 const int SRCLK_Pin = 6;
 const int numOfShiftRegisters = 1;
 RelayController relayController(SER_Pin, RCLK_Pin, SRCLK_Pin, numOfShiftRegisters);
 MotorController motorController(relayController);
-
+MQTTManager mqttManager(client, wifiManager, relayController, motorController, mqtt_server, mqtt_port);
 
 void mqttCallback(char* topic, byte* payload, unsigned int length) {
     payload[length] = '\0';  // Null-terminate the payload
     String payloadStr = String((char*)payload);
+    bool isOn = (payloadStr == "true");
 
     if (strcmp(topic, "/relay/monitor") == 0) {
-        if (payloadStr == "on") {
-            relayController.setRelayState(RelayController::Monitor, true);
-        } else if (payloadStr == "off") {
-            relayController.setRelayState(RelayController::Monitor, false);
-        }
+        relayController.setRelayState(RelayController::Relay::MONITOR, isOn);
     } else if (strcmp(topic, "/relay/laptop") == 0) {
-        if (payloadStr == "on") {
-            relayController.setRelayState(RelayController::Laptop, true);
-        } else if (payloadStr == "off") {
-            relayController.setRelayState(RelayController::Laptop, false);
-        }
-    } else if (strcmp(topic, "/relay/reserve1") == 0) {
-        if (payloadStr == "on") {
-            relayController.setRelayState(RelayController::Reserve1, true);
-        } else if (payloadStr == "off") {
-            relayController.setRelayState(RelayController::Reserve1, false);
-        }
+        relayController.setRelayState(RelayController::Relay::LAPTOP, isOn);
+    } else if (strcmp(topic, "/relay/fan") == 0) {
+        relayController.setRelayState(RelayController::Relay::FAN, isOn);
     } else if (strcmp(topic, "/relay/reserve2") == 0) {
-        if (payloadStr == "on") {
-            relayController.setRelayState(RelayController::Reserve2, true);
-        } else if (payloadStr == "off") {
-            relayController.setRelayState(RelayController::Reserve2, false);
-        }
+        relayController.setRelayState(RelayController::Relay::RESERVE2, isOn);
     } else if (strcmp(topic, "/motor/table") == 0) {
-        if (payloadStr == "up") {
-            motorController.moveTable(MotorController::Direction::Up);
-        } else if (payloadStr == "down") {
-            motorController.moveTable(MotorController::Direction::Down);
-        } else if (payloadStr == "stop") {
-            motorController.stopTable();
+        if (isOn) {
+            motorController.moveTableUp();
+        } else {
+            motorController.moveTableDown();
         }
-    } else if (strcmp(topic, "/motor/runtime") == 0) {
+    } else if (strcmp(topic, "/motor/runtimeup") == 0) {
         int runtime = payloadStr.toInt();
-        motorController.setRuntime(runtime);
-    }
-
-    // Feedback an den MQTT-Server senden
-    else {
-    mqttManager.publish("/error", ("Unknown command on topic: " + String(topic) + " with payload: " + payloadStr).c_str());
+        motorController.setMotorRunTimeUp(runtime);
+    } else if (strcmp(topic, "/motor/runtimedown") == 0) {
+        int runtime = payloadStr.toInt();
+        motorController.setMotorRunTimeDown(runtime);
+    } else {
+        mqttManager.publish("/error", ("Unknown command on topic: " + String(topic) + " with payload: " + payloadStr).c_str());
     }
 }
+
 
 void setup() {
     // Initialisierungen hier
