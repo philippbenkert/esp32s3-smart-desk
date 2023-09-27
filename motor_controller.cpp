7204945d2a842b7dd7f0df9009322cd27ca42641
philippbenkert <76922498+philippbenkert@users.noreply.github.com>
2023-09-27 22:11:46 +0200

    Update motor_controller.cpp

--- motor_controller.cpp
+++ motor_controller.cpp
@@ -1,36 +1,36 @@
-
 #include "motor_controller.h"
 
 MotorController::MotorController(RelayController& relayController)
-    : _relayController(relayController), _motorRunTimeUp(0), _motorRunTimeDown(0) {}
+    : _relayController(relayController), _motorRunTimeUp(0), _motorRunTimeDown(0), _currentState(IDLE), _startTime(0) {}
+
+unsigned long MotorController::getMotorRunTimeUp() const {
+    return _motorRunTimeUp;
+}
+
+unsigned long MotorController::getMotorRunTimeDown() const {
+    return _motorRunTimeDown;
+}
 
 void MotorController::setup() {
-    // Hier können Sie Initialisierungscode hinzufügen, falls erforderlich.
+    // Initialization code if needed
 }
 
 void MotorController::moveTableUp() {
-    // Stellen Sie sicher, dass das "runter"-Relais deaktiviert ist
     _relayController.setRelayState(RelayController::TABLE_DOWN, false);
-    delay(50);  // Ein kurzes Delay, um sicherzustellen, dass das Relais vollständig deaktiviert ist
+    _currentState = WAITING_TO_MOVE_UP;
+    _startTime = millis();
+}
 
-    _relayController.setRelayState(RelayController::TABLE_UP, true);
-    delay(_motorRunTimeUp);
+void MotorController::moveTableDown() {
     _relayController.setRelayState(RelayController::TABLE_UP, false);
+    _currentState = WAITING_TO_MOVE_DOWN;
+    _startTime = millis();
 }
 
 void MotorController::stopTable() {
     _relayController.setRelayState(RelayController::TABLE_UP, false);
     _relayController.setRelayState(RelayController::TABLE_DOWN, false);
-}
-
-void MotorController::moveTableDown() {
-    // Stellen Sie sicher, dass das "hoch"-Relais deaktiviert ist
-    _relayController.setRelayState(RelayController::TABLE_UP, false);
-    delay(50);  // Ein kurzes Delay, um sicherzustellen, dass das Relais vollständig deaktiviert ist
-
-    _relayController.setRelayState(RelayController::TABLE_DOWN, true);
-    delay(_motorRunTimeDown);
-    _relayController.setRelayState(RelayController::TABLE_DOWN, false);
+    _currentState = IDLE;
 }
 
 void MotorController::setMotorRunTimeUp(unsigned long duration) {
@@ -39,4 +39,43 @@ void MotorController::setMotorRunTimeUp(unsigned long duration) {
 
 void MotorController::setMotorRunTimeDown(unsigned long duration) {
     _motorRunTimeDown = duration;
+}
+
+void MotorController::update() {
+    unsigned long elapsed = millis() - _startTime;
+
+    switch (_currentState) {
+        case WAITING_TO_MOVE_UP:
+            if (elapsed >= 50) {
+                _relayController.setRelayState(RelayController::TABLE_UP, true);
+                _startTime = millis();
+                _currentState = MOVING_UP;
+            }
+            break;
+
+        case MOVING_UP:
+            if (elapsed >= _motorRunTimeUp) {
+                _relayController.setRelayState(RelayController::TABLE_UP, false);
+                _currentState = IDLE;
+            }
+            break;
+
+        case WAITING_TO_MOVE_DOWN:
+            if (elapsed >= 50) {
+                _relayController.setRelayState(RelayController::TABLE_DOWN, true);
+                _startTime = millis();
+                _currentState = MOVING_DOWN;
+            }
+            break;
+
+        case MOVING_DOWN:
+            if (elapsed >= _motorRunTimeDown) {
+                _relayController.setRelayState(RelayController::TABLE_DOWN, false);
+                _currentState = IDLE;
+            }
+            break;
+
+        default:
+            break;
+    }
 }
