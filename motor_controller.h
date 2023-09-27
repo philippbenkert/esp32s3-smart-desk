4cbe378b1836115220486d615e00075fce0e75a6
philippbenkert <76922498+philippbenkert@users.noreply.github.com>
2023-09-27 22:12:00 +0200

    Update motor_controller.h

--- motor_controller.h
+++ motor_controller.h
@@ -6,23 +6,30 @@
 class MotorController {
 public:
     MotorController(RelayController& relayController);
-
     void setup();
-
-    // Bewegt die Tischplatte nach oben oder unten
     void moveTableUp();
     void moveTableDown();
     void stopTable();
-
-    // Setzt die Laufzeit der Motoren
     void setMotorRunTimeUp(unsigned long duration);
     void setMotorRunTimeDown(unsigned long duration);
+    unsigned long getMotorRunTimeUp() const;
+    unsigned long getMotorRunTimeDown() const;
+    void update();
 
 private:
+    enum State {
+        IDLE,
+        WAITING_TO_MOVE_UP,
+        MOVING_UP,
+        WAITING_TO_MOVE_DOWN,
+        MOVING_DOWN
+    };
+
     RelayController& _relayController;
     unsigned long _motorRunTimeUp;
     unsigned long _motorRunTimeDown;
+    State _currentState;
+    unsigned long _startTime;
 };
 
 #endif // MOTOR_CONTROLLER_H
-
