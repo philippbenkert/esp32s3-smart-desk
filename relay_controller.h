49c348eed5bfbcce034048954d83b9033b8355ae
philippbenkert <76922498+philippbenkert@users.noreply.github.com>
2023-10-01 21:50:58 +0200

    Update relay_controller.h

--- relay_controller.h
+++ relay_controller.h
@@ -3,28 +3,42 @@
 
 #include <ShiftRegister74HC595.h>
 
+class MQTTManager;
+
 class RelayController {
+    MQTTManager* _mqttManager;
+
 public:
-    RelayController(int serPin, int rclkPin, int srclkPin, int numOfShiftRegisters);
-
+    RelayController(int serPin, int rclkPin, int srclkPin, int numOfShiftRegisters, MQTTManager* mqttManager = nullptr);
+    void setMQTTManager(MQTTManager* mqttManager);
     void setup();
-
+    unsigned long getKofferraumDuration() const;
+    void setKofferraumDuration(unsigned long duration);
     void setRelayState(int relayIndex, bool state);
     bool getRelayState(int relayIndex) const;
+    void update(); 
+    void setSsrFanPwm(int pwmValue);
 
-    enum Relay {
-        MONITOR = 0,
-        LAPTOP,
-        TABLE_UP,
-        TABLE_DOWN,
-        FAN,
-        RESERVE2
-    };
+    enum class Relay {
+    MONITOR,
+    LAPTOP,
+    TABLE_UP,
+    TABLE_DOWN,
+    FAN,
+    SSR_FAN,  // Hinzufügen dieser Zeile
+    KOFFERRAUM,
+    NUM_RELAYS
+};
 
 private:
     ShiftRegister74HC595<1> _shiftRegister;
-    std::array<bool, 6> _relayStates;
+    std::array<bool, static_cast<int>(Relay::NUM_RELAYS)> _relayStates;
+    unsigned long _kofferraumDuration = 0; // Zeitdauer für KOFFERRAUM
+    unsigned long _kofferraumStartTime = 0;
+    static constexpr int SSR_FAN_PIN = 8;
+    static constexpr int PWM_MIN = 0; // 1% entspricht 30 PWM
+    static constexpr int PWM_MAX = 255; // 100% entspricht 255 PWM
+    int _ssrFanPwmValue = 0; // Aktueller PWM-Wert für den SSR-Fan
 };
 
 #endif // RELAY_CONTROLLER_H
-
