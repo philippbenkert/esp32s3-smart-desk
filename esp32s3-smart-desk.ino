db8b7c023cc1da006eef34bdb53183b680442726
philippbenkert <76922498+philippbenkert@users.noreply.github.com>
2023-10-01 21:49:08 +0200

    Update esp32s3-smart-desk.ino

--- esp32s3-smart-desk.ino
+++ esp32s3-smart-desk.ino
@@ -4,47 +4,36 @@
 #include "motor_controller.h"
 #include <PubSubClient.h>
 
+constexpr char* ssid = "ssid";
+constexpr char* password = "password";
+WiFiManager wifiManager(ssid, password);
+constexpr char* mqtt_server = "192.168.0.1";
+constexpr int mqtt_port = 1024;
+constexpr char* client_id = "ESP32S3_Schreibtisch";
+constexpr int SER_Pin = 7;
+constexpr int RCLK_Pin = 5;
+constexpr int SRCLK_Pin = 6;
+constexpr int numOfShiftRegisters = 1;
+
 WiFiClient espClient;
 PubSubClient client(espClient);
-const char* ssid = "ssid";
-const char* password = "password";
-WiFiManager wifiManager(ssid, password);
-const char* mqtt_server = "192.168.0.1";
-const int mqtt_port = 1024;
-const char* client_id = "ESP32S3_Schreibtisch";
-const int SER_Pin = 7;
-const int RCLK_Pin = 5;
-const int SRCLK_Pin = 6;
-const int numOfShiftRegisters = 1;
-unsigned long previousMillis = 0;
-unsigned long motorRunTime = 0;
-bool isMovingUp = false;
-bool isMovingDown = false;
+
 RelayController relayController(SER_Pin, RCLK_Pin, SRCLK_Pin, numOfShiftRegisters);
 MotorController motorController(relayController);
 MQTTManager mqttManager(client, wifiManager, relayController, motorController, mqtt_server, mqtt_port);
 
 void setup() {
-    // Initialisierungen hier
     Serial.begin(115200);
     wifiManager.setup();
     mqttManager.setup();
+    relayController.setMQTTManager(&mqttManager);
     relayController.setup();
 }
 
 void loop() {
-    // Hauptlogik hier
     wifiManager.loop();
     mqttManager.loop();
     motorController.update();
+    relayController.update();
 
-    // Timer-Logik für Tischbewegung
-    unsigned long currentMillis = millis();
-    if (isMovingUp && (currentMillis - previousMillis >= motorRunTime)) {
-        motorController.stopTable();
-        isMovingUp = false;
-    } else if (isMovingDown && (currentMillis - previousMillis >= motorRunTime)) {
-        motorController.stopTable();
-        isMovingDown = false;
-    }
 }
